a9f63cd3893c846dbc0e58d59a317df022dc0505
Marcel Moritz <info@maretz.eu>
2014-06-08 12:50:33 +0200

    Update lastnews.php
    Ansicht Lastnews in list-group geändert

--- include/boxes/lastnews.php
+++ include/boxes/lastnews.php
@@ -13,13 +13,13 @@ $abf = 'SELECT
           LEFT JOIN prefix_user as b ON a.user_id = b.id
           WHERE news_recht >= '.$_SESSION['authright'].'
           ORDER BY a.news_time DESC
-          LIMIT 0,5';
-echo '<div class="panel panel-default"><div class="panel-body text-left">';        
+          LIMIT 0,4';
+echo '<ul class="list-group list-group-boxen text-left">';        
 $erg = db_query($abf);
 while ($row = db_fetch_object($erg)) {
 
-	echo '<div class="lastbox"><small>'.$row->kate.'</small><br><i class="fa fa-angle-double-right"></i> <a href="index.php?news-'.$row->id.'">'.$row->title.'</a><br><small>Autor : '.$row->username.' | '.$row->datum.'</small></div>';
+	echo '<a href="index.php?news-'.$row->id.'" class="list-group-item"><p class="text-muted">Kategorie: '.$row->kate.'</p><h4><i class="fa fa-angle-double-right"></i> '.$row->title.'</h4><p class="text-muted">Autor : '.$row->username.' | '.$row->datum.'</p></a>';
 
 }
-echo '</div></div>';
+echo '</ul>';
 ?>
